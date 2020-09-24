7ea68298bdbbd7e6193b4b0608c13ce6eaa7744c
Mark Gu <67448433+markgu713@users.noreply.github.com>
2020-09-23 23:33:14 -0400

    final scripts and outputs

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "b11a8be4c08930980713cb0636ac5a04"
+
+# Google API Key
+g_key = "AIzaSyBAYyh9e-Y5g2qrgWYlm0FiNv4AwjV7e3Y"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "b11a8be4c08930980713cb0636ac5a04"
+
+# Google API Key
+g_key = "AIzaSyBlzDRoRASKnRO9nowMVpVeGLRBVp5W9Zk"
